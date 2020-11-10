ccf1a62bcb0c73db69cbdcfc7f6a3d2f0e1306b5
Victor Bourman <68653187+bourman@users.noreply.github.com>
2020-11-10 16:22:41 +0200

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-8-jdk
+wget -q -o - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw status
+sudo ufw allow ssh
+sudo ufw allow 2222/tcp
