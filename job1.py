0aaa7a53e7e7e7ab52936c6ee63ee1a7fe053baf
Victor Bourman <Victorbourman77@gmail.com>
2023-08-31 01:51:34 +0300

    Update job1.py

--- job1.py
+++ job1.py
@@ -12,9 +12,9 @@ for r in response['Reservations']:
         print("ID: " + i['InstanceId'] + "\nIP Address: " + i['PublicIpAddress'] + "\n---------------------------------------")
 #Part-2
 print("Install the packages!")
-os.system('scp -i ./net4u_new.pem ./victor.sh ubuntu@18.188.205.77:/home/ubuntu')
-os.system('ssh -i "net4u_new.pem" ubuntu@ec2-18-188-205-77.us-east-2.compute.amazonaws.com')
+os.system('scp -i ./net4u_new.pem ./victor.sh ubuntu@---ipadd---:/home/ubuntu')
+os.system('ssh -i "net4u_new.pem" ubuntu@--------.us-east-2.compute.amazonaws.com')
 print('Run victor.sh')
 os.system('scp -i ./net4u_new.pem ./victor.sh ubuntu@3.14.70.202:/home/ubuntu')
-os.system('ssh -i "net4u_new.pem" ubuntu@ec2-3-14-70-202.us-east-2.compute.amazonaws.com')
+os.system('ssh -i "net4u_new.pem" ubuntu@--------.us-east-2.compute.amazonaws.com')
 print('Run victor.sh')
